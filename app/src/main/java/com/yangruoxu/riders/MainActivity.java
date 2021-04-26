672b9c2ccfcd26d4814a075ce81f452e71579376
squirrelzl <51698990+squirrelzl@users.noreply.github.com>
2021-04-26 13:55:40 +0800

    Update MainActivity.java

--- app/src/main/java/com/yangruoxu/riders/MainActivity.java
+++ app/src/main/java/com/yangruoxu/riders/MainActivity.java
@@ -1,14 +1,23 @@
 package com.yangruoxu.riders;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,6 +25,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.TextView;
@@ -39,6 +50,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
 
+import android.provider.Settings;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
@@ -52,12 +66,27 @@ public class MainActivity extends AppCompatActivity {
     //private String imei = UUID.randomUUID().toString().replace("-","");
     //private String imei = getPhoneIMEI();
     private Timer timer;
+    /*Intent intent = new Intent(MainActivity.this, MyService.class);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        startForegroundService(intent);
+    } else {
+        startService(intent);
+    }*/
+
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //positionTextView = (TextView) findViewById(R.id.postion_text);
+        //positionTextView.setText("start serve!");
+        /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //android8.0以上通过startForegroundService启动service
+            startForegroundService(new Intent(MainActivity.this, MyService.class));
+        }else{
+            startService(new Intent(MainActivity.this, MyService.class));
+        }*/
         StartTime = getCurrTime();
         runnable = new Runnable() {
             @Override
@@ -68,6 +97,15 @@ public class MainActivity extends AppCompatActivity {
         };
         handler = new Handler();
         handler.postDelayed(runnable, 0);
+        //intent = new Intent(MainActivity.this, ForegroundService.class);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //android8.0以上通过startForegroundService启动service
+            startForegroundService(new Intent(MainActivity.this, MyService.class));
+        }else{
+            startService(new Intent(MainActivity.this, MyService.class));
+        }
+
+
 //        while(true){
 //            geolocation();
 //            try {
@@ -76,9 +114,117 @@ public class MainActivity extends AppCompatActivity {
 //                e.printStackTrace();
 //            }
 //        }
-
-
     }
+    /*public class MyService extends Service{
+
+        @SuppressLint("HandlerLeak")
+        private Handler mHandler=new Handler(){
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                if(mBuilder!=null){
+                    Log.i(TAG, "handleMessage: -------2");
+                    startForeground(10, mBuilder.build());
+                }
+                sendEmptyMessageDelayed(0,3*60*1000);
+            }
+        };
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+
+            Log.i(TAG, "onStartCommand: -----");
+            if (mBuilder == null) {
+                mBuilder = new NotificationCompat.Builder(this,"10");
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService
+                        (NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("10", "senblo", NotificationManager.IMPORTANCE_DEFAULT);
+//                mBuilder.setChannelId("10");
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                mBuilder.setLargeIcon(BitmapFactory.decodeResource(BaseApplication.getApplication().getResources(), R.mipmap.ic_launcher_app));
+                mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+                mBuilder.setContentText("senblo sport running");
+                mBuilder.setContentTitle("senblo sport");
+                mBuilder.setOnlyAlertOnce(true);
+
+                Intent newIntent = new Intent(this, MainActivity.class);
+                newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                mBuilder.setContentIntent(PendingIntent.getActivity(this, 10, newIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+                mBuilder.build().flags |= Notification.FLAG_ONGOING_EVENT;
+                mBuilder.build().flags |= Notification.FLAG_NO_CLEAR;
+            }
+
+            startForeground(10, mBuilder.build());
+
+
+            return START_STICKY;
+
+        }
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            Log.i(TAG, "onCreate: --------");
+
+            mHandler.sendEmptyMessageDelayed(0,5000);
+        }
+
+    }*/
+
+    /*public class MyService extends Service{
+        private NotificationManager notificationManager;
+        private String notificationId = "serviceid";
+        private String notificationName = "servicename";
+
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            //创建NotificationChannel
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                NotificationChannel channel = new NotificationChannel(notificationId, notificationName, NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(channel);
+            }
+            startForeground(1,getNotification());
+
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+        private Notification getNotification() {
+            Notification.Builder builder = new Notification.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("title")
+                    .setContentText("text");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                builder.setChannelId(notificationId);
+            }
+            Notification notification = builder.build();
+            return notification;
+        }
+
+    }*/
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void geolocation() {
@@ -161,7 +307,7 @@ public class MainActivity extends AppCompatActivity {
         Location location = locationManager.getLastKnownLocation(provider);
 
         if (location != null) {
-            save(location, getCurrTime());
+            //save(location, getCurrTime());
             showLocation(location);
         }
         locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 1000, 0, locationListener);
@@ -189,6 +335,7 @@ public class MainActivity extends AppCompatActivity {
             //save(location, getCurrTime());
             //showLocation(location);
         }
+
     };
 
     private void showLocation(Location location) {
@@ -197,9 +344,7 @@ public class MainActivity extends AppCompatActivity {
                 "longitude is: " + location.getLongitude() + "\n" + "speed is: " + location.getSpeed() + "\n" + provider + "\n" + "accuracy: " + location.getAccuracy() + "\n" + location.getTime();
         positionTextView.setText(currentPostion);
         return;
-    }
-
-    ;
+    };
 
 
     public void save(Location location, String CurrentTime) {
@@ -207,7 +352,8 @@ public class MainActivity extends AppCompatActivity {
         BufferedWriter writer = null;
         File file = null;
 
-        String imei = UUID.randomUUID().toString().replace("-","");
+        //String imei = UUID.randomUUID().toString().replace("-","");
+        String imei = Settings.System.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
         //String imei = getPhoneIMEI();
         data = imei + "|" + provider + ":" + CurrentTime + ";" + location.getLongitude() + "," + location.getLatitude() + ";" + location.getSpeed() + ";" + location.getAccuracy() + "\n";
         new Thread(new Runnable() {
@@ -288,14 +434,16 @@ public class MainActivity extends AppCompatActivity {
                 //                                          int[] grantResults)
                 // to handle the case where the user grants the permission. See the documentation
                 // for Activity#requestPermissions for more details.
-                String b =  tm.getImei();
-                return b;
-
+                //String b =  tm.getImei();
+                //return b;
+                //return Settings.System.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+                return "111111";
             }
         }
         //String b = tm.getLine1Number();
-        return tm.getImei();
-        //return "";
+        //return tm.getImei();
+        //return Settings.System.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        return "000000";
     };
 
     public String getCurrTime(){
@@ -303,6 +451,7 @@ public class MainActivity extends AppCompatActivity {
         Date date = new Date(System.currentTimeMillis());
         return simpleDateFormat.format(date);
     }
+
 
 
 }
